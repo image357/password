96a896e98edd88e2d3631643b4d84c9c8546c98c
Marcel Koepke <marcel.koepke@gmail.com>
2024-12-13 21:07:47 +0100

    add storage GetStorePath tests

--- storage_test.go
+++ storage_test.go
@@ -1,6 +1,7 @@
 package password
 
 import (
+	"path/filepath"
 	"testing"
 )
 
@@ -53,6 +54,34 @@ func Test_NormalizeId(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := NormalizeId(tt.args.path); got != tt.want {
 				t.Errorf("NormalizeId() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetStorePath(t *testing.T) {
+	tests := []struct {
+		name    string
+		backend Storage
+		want    string
+		wantErr bool
+	}{
+		{"pass", &FileStorage{storePath: "foo/bar", fileEnding: "pwd"}, filepath.FromSlash("foo/bar"), false},
+		{"fail", nil, filepath.FromSlash(""), true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			RegisterDefaultManager("old")
+			currentManager := GetDefaultManager()
+			currentManager.storageBackend = tt.backend
+			got, err := GetStorePath()
+			SetDefaultManager(Managers["old"])
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetStorePath() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("GetStorePath() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
