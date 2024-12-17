b1b813d378ceba69de0a5cb9915faf58f8b807db
Marcel Koepke <marcel.koepke@gmail.com>
2024-12-17 12:12:25 +0100

    add manager Overwrite tests

--- manager_test.go
+++ manager_test.go
@@ -1,6 +1,7 @@
 package password
 
 import (
+	"os"
 	"reflect"
 	"testing"
 )
@@ -102,5 +103,49 @@ func TestManager_getRecoveryKey(t *testing.T) {
 				t.Errorf("getRecoveryKey() = %v, want %v", got, tt.want)
 			}
 		})
+	}
+}
+
+func TestManager_Overwrite(t *testing.T) {
+	type args struct {
+		id       string
+		password string
+		key      string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{"create", args{"foo", "123", "456"}, false},
+		{"overwrite", args{"foo", "789", "abc"}, false},
+		{"create folder", args{"foo/bar", "123", "456"}, false},
+		{"overwrite folder", args{"foo/bar", "789", "abc"}, false},
+		{"create subfolder", args{"bar/baz/foo", "123", "456"}, false},
+		{"overwrite subfolder", args{"bar/baz/foo", "789", "abc"}, false},
+		{"add subfolder", args{"bar/boo/foo", "789", "abc"}, false},
+		{"create mixed slashes", args{"forward/backward\\foo", "123", "456"}, false},
+		{"overwrite mixed slashes", args{"forward\\backward/foo", "abc", "def"}, false},
+		{"stop recurse on recovery", args{"foo" + RecoveryIdSuffix, "123", "456"}, false},
+	}
+	// init
+	m := NewManager()
+	m.storageBackend.(*FileStorage).SetStorePath("./tests/workdir/Overwrite")
+	m.EnableRecovery("recovery_key")
+
+	// tests
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := m.Overwrite(tt.args.id, tt.args.password, tt.args.key); (err != nil) != tt.wantErr {
+				t.Errorf("Overwrite() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+
+	// cleanup
+	path := m.storageBackend.(*FileStorage).GetStorePath()
+	err := os.RemoveAll(path)
+	if err != nil {
+		t.Fatal(err)
 	}
 }
