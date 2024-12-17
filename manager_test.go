08093093eaf746868bde72b763df546dfa019331
Marcel Koepke <marcel.koepke@gmail.com>
2024-12-17 11:42:38 +0100

    add manager EnableRecovery tests

--- manager_test.go
+++ manager_test.go
@@ -24,3 +24,38 @@ func TestNewManager(t *testing.T) {
 		})
 	}
 }
+
+func TestManager_EnableRecovery(t *testing.T) {
+	type args struct {
+		key string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{"enable_recovery", args{"123456"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := NewManager()
+			if len(m.recoveryKeyBytes) != 0 {
+				t.Fatalf("NewManager() should not have any recovery key")
+			}
+			if len(m.recoveryKeySecret) != 0 {
+				t.Fatalf("NewManager() should not have any recovery secret")
+			}
+
+			m.EnableRecovery(tt.args.key)
+
+			if !m.withRecovery {
+				t.Fatalf("manager should have recovery enabled")
+			}
+			if len(m.recoveryKeyBytes) == 0 {
+				t.Errorf("wrong recovery key length = %v", len(m.recoveryKeyBytes))
+			}
+			if len(m.recoveryKeySecret) == 0 {
+				t.Errorf("wrong recovery secret length = %v", len(m.recoveryKeySecret))
+			}
+		})
+	}
+}
