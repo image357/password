4b9641649783f7fc8a65fb4c1d92f324beec1f38
Marcel Koepke <marcel.koepke@gmail.com>
2024-12-18 15:23:04 +0100

    add encryption helper tool

---
+++ cmd/encrypt/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"github.com/image357/password"
+	"os"
+	"unicode/utf8"
+)
+
+func main() {
+	args := os.Args[1:]
+	if len(args) != 2 {
+		fmt.Println("Usage: encrypt <file> <key>")
+		os.Exit(1)
+	}
+
+	filePath := args[0]
+	storageKey := args[1]
+
+	// read file
+	fileContents, err := os.ReadFile(filePath)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		os.Exit(1)
+	}
+	if !utf8.Valid(fileContents) {
+		fmt.Println("invalid utf8 character after file reading")
+		os.Exit(1)
+	}
+	data := string(fileContents)
+
+	// encrypt data
+	encryptedData, err := password.Encrypt(data, storageKey)
+	if err != nil {
+		fmt.Println("Error encrypting file:", err)
+		os.Exit(1)
+	}
+
+	// print data
+	fmt.Printf("%v", string(encryptedData))
+}
