ff50b1f8936e7a0bc045c953cb5f3273aa3129cf
Marcel Koepke <marcel.koepke@gmail.com>
2024-12-18 15:18:59 +0100

    add decryption helper tool

---
+++ cmd/decrypt/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"github.com/image357/password"
+	"os"
+	"unicode/utf8"
+)
+
+func main() {
+	args := os.Args[1:]
+	if len(args) != 2 {
+		fmt.Println("Usage: decrypt <file> <key>")
+		os.Exit(1)
+	}
+
+	filePath := args[0]
+	storageKey := args[1]
+
+	// read file
+	fileContents, err := os.ReadFile(filePath)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		os.Exit(1)
+	}
+	if !utf8.Valid(fileContents) {
+		fmt.Println("invalid utf8 character after file reading")
+		os.Exit(1)
+	}
+	encryptedData := string(fileContents)
+
+	// decrypt data
+	packedData, err := password.Decrypt(encryptedData, storageKey)
+	if err != nil {
+		fmt.Println("Error decrypting file:", err)
+		os.Exit(1)
+	}
+
+	// print data
+	fmt.Printf("%v", string(packedData))
+}
