9949ee23bd6e6b37d71907377c0fb69e1d786b96
Marcel Koepke <marcel.koepke@gmail.com>
2024-12-18 19:23:53 +0100

    add temporary Store tests

--- temporary_test.go
+++ temporary_test.go
@@ -24,3 +24,34 @@ func TestNewTemporaryStorage(t *testing.T) {
 		})
 	}
 }
+
+func TestTemporaryStorage_Store(t1 *testing.T) {
+	type args struct {
+		id   string
+		data string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{"first", args{id: "first", data: "first"}, false},
+		{"second", args{id: "second", data: "second"}, false},
+		{"third", args{id: "third", data: "third"}, false},
+	}
+	// init
+	t := NewTemporaryStorage()
+
+	// tests
+	for _, tt := range tests {
+		t1.Run(tt.name, func(t1 *testing.T) {
+			if err := t.Store(tt.args.id, tt.args.data); (err != nil) != tt.wantErr {
+				t1.Errorf("Store() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			_, ok := t.registry[tt.args.id]
+			if !ok {
+				t1.Errorf("id not found: %v", tt.args.id)
+			}
+		})
+	}
+}
